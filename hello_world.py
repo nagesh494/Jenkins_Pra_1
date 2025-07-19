d83d17d39748147b9ba7d349a2be3c39d7c9bf23
segireddyr <ranganageswarareddy.s@gmail.com>
2025-07-19 04:44:23 +0000

    added date time

--- hello_world.py
+++ hello_world.py
@@ -1,4 +1,14 @@
 import time
+from datetime import date
 
 print("Hello world")
 print("Hi My Name is Nagesh")
+print(date.today())
+
+def print_date():
+    print("Entered to the function")
+    mt_time = date.today()
+    print(f'Today date is : {mt_time}')
+
+if __name__ == '__main__':
+    print_date()
